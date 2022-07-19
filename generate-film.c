721eb128ab5e10056edecf3b5b7265e018bae356
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-07-19 16:30:50 +0200

    generate-film: Look for files relative to exe, not cwd

--- generate-film.c
+++ generate-film.c
@@ -17,9 +17,16 @@ struct child_proc {
 #define CHILD_PROC_INIT { .pid = -1, .read_fd = -1 }
 
 static bool
-open_child_proc(char *const argv[],
+open_child_proc(const char *source_dir,
+                const char *program_name,
+                const char *const argv[],
                 struct child_proc *cp)
 {
+        int n_args = 0;
+
+        for (const char *const *p = argv; *p; p++)
+                n_args++;
+
         int pipe_fds[2];
 
         if (pipe(pipe_fds) == -1) {
@@ -43,11 +50,27 @@ open_child_proc(char *const argv[],
                         fprintf(stderr, "dup2 failed: %s\n", strerror(errno));
                 } else {
                         close(pipe_fds[1]);
-                        execvp(argv[0], argv);
+
+                        const char **argv_copy =
+                                flt_alloc((n_args + 2) *
+                                          sizeof (const char *));
+                        char *full_program = flt_strconcat(source_dir,
+                                                           "/",
+                                                           program_name,
+                                                           NULL);
+                        argv_copy[0] = full_program;
+                        memcpy(argv_copy + 1,
+                               argv, (n_args + 1) * sizeof (const char *));
+
+                        execvp(full_program, (char **) argv_copy);
+
                         fprintf(stderr,
                                 "exec failed: %s: %s\n",
-                                argv[0],
+                                full_program,
                                 strerror(errno));
+
+                        flt_free(argv_copy);
+                        flt_free(full_program);
                 }
 
                 exit(EXIT_FAILURE);
@@ -84,11 +107,13 @@ close_child_proc(struct child_proc *cp)
 }
 
 static char *
-get_process_output(char *const argv[])
+get_process_output(const char *source_dir,
+                   const char *program_name,
+                   const char *const argv[])
 {
         struct child_proc cp;
 
-        if (!open_child_proc(argv, &cp))
+        if (!open_child_proc(source_dir, program_name, argv, &cp))
                 return NULL;
 
         struct flt_buffer buf = FLT_BUFFER_STATIC_INIT;
@@ -116,18 +141,18 @@ get_process_output(char *const argv[])
 }
 
 static bool
-get_speedy_args(const char *speedy_file,
+get_speedy_args(const char *source_dir,
+                const char *speedy_file,
                 struct flt_buffer *buf,
                 int *n_inputs,
                 char **filter_arg_out)
 {
-        char *proc_args[] = {
-                "./speedy.py",
+        const char * const proc_args[] = {
                 (char *) speedy_file,
                 NULL,
         };
 
-        char *output = get_process_output(proc_args);
+        char *output = get_process_output(source_dir, "speedy.py", proc_args);
 
         if (output == NULL)
                 return false;
@@ -174,7 +199,8 @@ add_arg(struct flt_buffer *args, const char *arg)
 }
 
 static void
-add_ffmpeg_args(struct flt_buffer *args,
+add_ffmpeg_args(const char *source_dir,
+                struct flt_buffer *args,
                 int n_inputs,
                 const char *filter_arg,
                 struct child_proc *logo_proc,
@@ -201,7 +227,10 @@ add_ffmpeg_args(struct flt_buffer *args,
         add_arg(args, (const char *) buf.data);
 
         add_arg(args, "-i");
-        add_arg(args, "logo-sound.flac");
+
+        char *logo_sound_file =
+                flt_strconcat(source_dir, "/logo-sound.flac", NULL);
+        flt_buffer_append(args, &logo_sound_file, sizeof logo_sound_file);
 
         add_arg(args, "-f");
         add_arg(args, "rawvideo");
@@ -304,6 +333,25 @@ free_args(struct flt_buffer *buf)
         flt_buffer_destroy(buf);
 }
 
+static char *
+get_source_dir(const char *exe)
+{
+        const char *end = strrchr(exe, '/');
+
+        if (end == NULL)
+                return flt_strdup(".");
+
+        static const char build_end[] = "/build";
+
+        if (end - exe >= sizeof build_end &&
+            !memcmp(build_end,
+                    end - (sizeof build_end) + 1,
+                    (sizeof build_end) - 1))
+                end -= (sizeof build_end) - 1;
+
+        return flt_strndup(exe, end - exe);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -321,6 +369,8 @@ main(int argc, char **argv)
         char *filter_arg = NULL;
         int n_inputs;
 
+        char *source_dir = get_source_dir(argv[0]);
+
         int ret = EXIT_SUCCESS;
 
         add_arg(&args, "ffmpeg");
@@ -328,31 +378,41 @@ main(int argc, char **argv)
         struct child_proc logo_proc = CHILD_PROC_INIT;
         struct child_proc flootay_proc = CHILD_PROC_INIT;
 
-        if (!get_speedy_args(speedy_file, &args, &n_inputs, &filter_arg)) {
+        if (!get_speedy_args(source_dir,
+                             speedy_file,
+                             &args,
+                             &n_inputs,
+                             &filter_arg)) {
                 ret = EXIT_FAILURE;
                 goto out;
         }
 
-        char *logo_args[] = { "./build/generate-logo", NULL };
+        static const char * const logo_args[] = { NULL };
 
-        if (!open_child_proc(logo_args, &logo_proc)) {
+        if (!open_child_proc(source_dir,
+                             "build/generate-logo",
+                             logo_args,
+                             &logo_proc)) {
                 ret = EXIT_FAILURE;
                 goto out;
         }
 
-        char *flootay_args[] = {
-                "./build/flootay",
-                (char *) flootay_file,
+        const char * const flootay_args[] = {
+                flootay_file,
                 "scores.flt",
                 NULL
         };
 
-        if (!open_child_proc(flootay_args, &flootay_proc)) {
+        if (!open_child_proc(source_dir,
+                             "build/flootay",
+                             flootay_args,
+                             &flootay_proc)) {
                 ret = EXIT_FAILURE;
                 goto out;
         }
 
-        add_ffmpeg_args(&args,
+        add_ffmpeg_args(source_dir,
+                        &args,
                         n_inputs,
                         filter_arg,
                         &logo_proc,
@@ -370,6 +430,8 @@ out:
 
         free_args(&args);
         flt_free(filter_arg);
+
+        flt_free(source_dir);
 
         return ret;
 }

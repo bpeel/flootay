c01ac2b454854961b4210dddbb0f0e8f1b67500c
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-07-25 10:43:18 +0200

    generate-sound: Automatically lower volume of overlapping sounds

--- generate-sound.c
+++ generate-sound.c
@@ -16,8 +16,12 @@
 #define SAMPLE_RATE 48000
 #define CHANNELS 2
 #define SAMPLE_SIZE 3
+#define SAMPLE_MAX_VALUE ((1 << (SAMPLE_SIZE * 8)) - 1)
 
 #define BUFFER_SIZE 4096
+
+#define VOLUME_SLIDE_TIME 1.0
+#define QUIET_VOLUME 0.1
 
 struct sound {
         struct flt_list link;
@@ -26,10 +30,41 @@ struct sound {
         char *filename;
 };
 
+struct running_sound {
+        struct flt_list link;
+        const struct sound *sound;
+        struct flt_child_proc cp;
+        FILE *f;
+};
+
 static const struct sound
 default_sound = {
         .start_time = 0.0,
 };
+
+static const struct flt_child_proc
+child_proc_init = FLT_CHILD_PROC_INIT;
+
+static void
+free_running_sound(struct running_sound *sound)
+{
+        if (sound->f)
+                fclose(sound->f);
+
+        flt_child_proc_close(&sound->cp);
+
+        flt_free(sound);
+}
+
+static void
+free_running_sounds(struct flt_list *list)
+{
+        struct running_sound *sound, *tmp;
+
+        flt_list_for_each_safe(sound, tmp, list, link) {
+                free_running_sound(sound);
+        }
+}
 
 static bool
 get_sound_length(const char *filename,
@@ -70,19 +105,72 @@ get_sound_length(const char *filename,
         return true;
 }
 
-static int
-copy_file_to_stdout(const char *filename)
+static double
+get_sound_sample_volume(const struct flt_list *sounds,
+                        const struct sound *sound,
+                        double sample_time)
 {
-        struct flt_child_proc cp = FLT_CHILD_PROC_INIT;
+        double max_volume = 1.0;
+
+        for (const struct sound *s = flt_container_of(sound->link.next,
+                                                      struct sound,
+                                                      link);
+             &s->link != sounds;
+             s = flt_container_of(s->link.next, struct sound, link)) {
+                if (sample_time < s->start_time) {
+                        if (sound->start_time + sound->length > s->start_time &&
+                            sample_time >= s->start_time - VOLUME_SLIDE_TIME) {
+                                float v = ((1.0 - ((sample_time +
+                                                    VOLUME_SLIDE_TIME -
+                                                    s->start_time) /
+                                                   VOLUME_SLIDE_TIME)) *
+                                           (1.0 - QUIET_VOLUME) +
+                                           QUIET_VOLUME);
+
+                                if (v < max_volume)
+                                        max_volume = v;
+                        }
+                } else if (sample_time >= s->start_time + s->length) {
+                        if (sound->start_time < s->start_time + s->length &&
+                            sample_time < (s->start_time +
+                                           s->length +
+                                           VOLUME_SLIDE_TIME)) {
+                                float v = (((sample_time -
+                                             s->start_time -
+                                             s->length) /
+                                            VOLUME_SLIDE_TIME) *
+                                           (1.0 - QUIET_VOLUME) +
+                                           QUIET_VOLUME);
+
+                                if (v < max_volume)
+                                        max_volume = v;
+                        }
+                } else {
+                        return QUIET_VOLUME;
+                }
+        }
+
+        return max_volume;
+}
+
+static bool
+start_sound(struct flt_list *running_sounds,
+            const struct sound *sound)
+{
+        struct running_sound *rs = flt_calloc(sizeof *rs);
+
+        rs->sound = sound;
+        rs->cp = child_proc_init;
 
         const char * const ffmpeg_args[] = {
-                "-i", filename,
+                "-i", sound->filename,
                 "-ar", FLT_STRINGIFY(SAMPLE_RATE),
                 "-ac", "2",
                 "-f", "s24le",
                 "-c:a", "pcm_s24le",
                 "-hide_banner",
                 "-loglevel", "error",
+                "-nostdin",
                 "-",
                 NULL,
         };
@@ -90,63 +178,98 @@ copy_file_to_stdout(const char *filename)
         if (!flt_child_proc_open(NULL, /* source_dir */
                                  "ffmpeg",
                                  ffmpeg_args,
-                                 &cp))
-                return -1;
-
-        int ret = 0;
-
-        uint8_t *buf = flt_alloc(BUFFER_SIZE);
-
-        while (true) {
-                ssize_t got = read(cp.read_fd, buf, BUFFER_SIZE);
-
-                if (got == 0)
-                        break;
-
-                if (got < 0) {
-                        fprintf(stderr,
-                                "error reading from ffmpeg: %s\n",
-                                strerror(errno));
-                        ret = -1;
-                        break;
-                }
-
-                size_t wrote = fwrite(buf, 1, got, stdout);
-
-                if (wrote < got) {
-                        fprintf(stderr,
-                                "error writing to stdout: %s\n",
-                                strerror(errno));
-                        ret = -1;
-                        break;
-                }
-
-                ret += got;
+                                 &rs->cp)) {
+                free_running_sound(rs);
+                return false;
         }
 
-        if (!flt_child_proc_close(&cp))
-                ret = -1;
+        rs->f = fdopen(rs->cp.read_fd, "r");
 
-        flt_free(buf);
+        if (rs->f == NULL) {
+                fprintf(stderr, "fdopen failed: %s\n", strerror(errno));
+                free_running_sound(rs);
+                return false;
+        }
 
-        return ret;
+        rs->cp.read_fd = -1;
+
+        flt_list_insert(running_sounds->prev, &rs->link);
+
+        return true;
 }
 
 static bool
-generate_silence(size_t n_samples)
+read_samples(struct running_sound *rs, double samples[CHANNELS])
 {
-        static const uint8_t zero_sample[CHANNELS * SAMPLE_SIZE] = { 0 };
+        for (int i = 0; i < CHANNELS; i++) {
+                uint8_t buf[SAMPLE_SIZE];
 
-        for (size_t i = 0; i < n_samples; i++) {
-                size_t wrote = fwrite(zero_sample,
-                                      1, sizeof zero_sample,
-                                      stdout);
-
-                if (wrote != sizeof zero_sample) {
-                        fprintf(stderr,
-                                "error writing to stdout: %s\n",
-                                strerror(errno));
+                if (fread(buf, 1, sizeof buf, rs->f) != sizeof buf)
                         return false;
+
+                int value = 0;
+
+                for (int j = 0; j < SAMPLE_SIZE; j++)
+                        value |= buf[j] << (8 * j);
+
+                if ((buf[SAMPLE_SIZE - 1] & 0x80))
+                        value |= -1 << (SAMPLE_SIZE * 8);
+
+                samples[i] = value / (double) SAMPLE_MAX_VALUE;
+        }
+
+        return true;
+}
+
+static bool
+get_samples(struct flt_list *running_sounds,
+            const struct flt_list *sounds,
+            double current_time,
+            double samples[CHANNELS])
+{
+        struct running_sound *rs, *tmp;
+
+        for (int i = 0; i < CHANNELS; i++)
+                samples[i] = 0.0;
+
+        flt_list_for_each_safe(rs, tmp, running_sounds, link) {
+                double sound_samples[CHANNELS];
+
+                if (!read_samples(rs, sound_samples)) {
+                        bool close_ret = flt_child_proc_close(&rs->cp);
+                        rs->cp = child_proc_init;
+                        if (!close_ret)
+                                return false;
+                        flt_list_remove(&rs->link);
+                        free_running_sound(rs);
+                        continue;
+                }
+
+                double volume = get_sound_sample_volume(sounds,
+                                                        rs->sound,
+                                                        current_time);
+
+                for (int i = 0; i < CHANNELS; i++)
+                        samples[i] += sound_samples[i] * volume;
+        }
+
+        return true;
+}
+
+static bool
+write_samples(const double samples[CHANNELS])
+{
+        for (int i = 0; i < CHANNELS; i++) {
+                int value = (samples[i] >= 1.0 ? SAMPLE_MAX_VALUE :
+                             samples[i] <= -1.0 ? -SAMPLE_MAX_VALUE :
+                             round(samples[i] * SAMPLE_MAX_VALUE));
+                for (int j = 0; j < SAMPLE_SIZE; j++) {
+                        if (fputc(value & 0xff, stdout) == EOF) {
+                                fprintf(stderr, "error writing to stdout\n");
+                                return false;
+                        }
+
+                        value >>= 8;
                 }
         }
 
@@ -157,26 +280,50 @@ static bool
 write_sounds(const struct flt_list *sounds)
 {
         size_t samples_written = 0;
-        const struct sound *sound;
+        const struct flt_list *next_sound_link = sounds->next;
+        struct flt_list running_sounds;
+        bool ret = true;
 
-        flt_list_for_each(sound, sounds, link) {
-                size_t start_samples = round(sound->start_time * SAMPLE_RATE);
+        flt_list_init(&running_sounds);
 
-                if (start_samples > samples_written &&
-                    !generate_silence(start_samples - samples_written))
-                        return false;
+        while (next_sound_link != sounds || !flt_list_empty(&running_sounds)) {
+                double current_time = samples_written / (double) SAMPLE_RATE;
 
-                samples_written = start_samples;
+                while (next_sound_link != sounds) {
+                        const struct sound *next_sound =
+                                flt_container_of(next_sound_link,
+                                                 struct sound,
+                                                 link);
 
-                int sound_samples = copy_file_to_stdout(sound->filename);
+                        if (next_sound->start_time > current_time)
+                                break;
 
-                if (sound_samples < 0)
-                        return false;
+                        if (!start_sound(&running_sounds, next_sound)) {
+                                ret = false;
+                                goto out;
+                        }
 
-                samples_written += sound_samples / (CHANNELS * SAMPLE_SIZE);
+                        next_sound_link = next_sound_link->next;
+                }
+
+                double samples[CHANNELS];
+
+                if (!get_samples(&running_sounds,
+                                 sounds,
+                                 current_time,
+                                 samples) ||
+                    !write_samples(samples)) {
+                        ret = false;
+                        goto out;
+                }
+
+                samples_written++;
         }
 
-        return true;
+out:
+        free_running_sounds(&running_sounds);
+
+        return ret;
 }
 
 static void

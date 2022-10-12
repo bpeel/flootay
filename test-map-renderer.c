a0b151b264b64e1f69b9ee23ce6b733cf0ab1def
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-10-12 09:32:20 +0200

    test-map-renderer: Make everything customisable with command line args

--- test-map-renderer.c
+++ test-map-renderer.c
@@ -18,49 +18,201 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <float.h>
+#include <getopt.h>
+#include <math.h>
 
 #include "flt-map-renderer.h"
 
-#define IMAGE_SIZE 512
+struct config {
+        double lat, lon;
+        int width, height;
+        int zoom;
+        bool clip;
+        const char *url_base;
+        const char *api_key;
+        const char *output_filename;
+};
+
+static bool
+parse_positive_int(const char *str, int *value_out)
+{
+        errno = 0;
+
+        char *tail;
+
+        long value = strtol(str, &tail, 10);
+
+        if (value <= 0 || value > INT_MAX || errno || *tail)
+                return false;
+
+        *value_out = value;
+
+        return true;
+}
+
+static bool
+parse_coordinate(const char *arg, struct config *config)
+{
+        const char *part;
+        double *value_out;
+        double min_value, max_value;
+
+        if (config->lat == DBL_MAX) {
+                value_out = &config->lat;
+                min_value = -90.0;
+                max_value = 90.0;
+                part = "latitude";
+        } else if (config->lon == DBL_MAX) {
+                value_out = &config->lon;
+                min_value = -180.0;
+                max_value = 180.0;
+                part = "longitude";
+        } else {
+                fprintf(stderr, "Too many coordinates specified\n");
+                return false;
+        }
+
+        errno = 0;
+        char *tail;
+        *value_out = strtod(optarg, &tail);
+
+        if (errno ||
+            (!isnormal(*value_out) && *value_out != 0.0) ||
+            *tail ||
+            *value_out < min_value ||
+            *value_out > max_value) {
+                fprintf(stderr,
+                        "invalid %s: %s\n",
+                        part,
+                        optarg);
+                return false;
+        }
+
+        return true;
+}
+
+static bool
+process_options(int argc, char **argv, struct config *config)
+{
+        config->lat = DBL_MAX;
+        config->lon = DBL_MAX;
+        config->width = 512;
+        config->height = 512;
+        config->zoom = 17;
+        config->clip = false;
+        config->output_filename = "map.png";
+        config->url_base = NULL;
+        config->api_key = NULL;
+
+        while (true) {
+                switch (getopt(argc, argv, "-w:h:z:cu:a:o:")) {
+                case 'w':
+                        if (!parse_positive_int(optarg, &config->width)) {
+                                fprintf(stderr,
+                                        "invalid width: %s\n",
+                                        optarg);
+                                return false;
+                        }
+                        break;
+                case 'h':
+                        if (!parse_positive_int(optarg, &config->height)) {
+                                fprintf(stderr,
+                                        "invalid height: %s\n",
+                                        optarg);
+                                return false;
+                        }
+                        break;
+                case 'z':
+                        if (!parse_positive_int(optarg, &config->zoom)) {
+                                fprintf(stderr,
+                                        "invalid zoom: %s\n",
+                                        optarg);
+                                return false;
+                        }
+                        break;
+                case 'c':
+                        config->clip = true;
+                        break;
+                case 'u':
+                        config->url_base = optarg;
+                        break;
+                case 'a':
+                        config->api_key = optarg;
+                        break;
+                case 'o':
+                        config->output_filename = optarg;
+                        break;
+                case 1:
+                        if (!parse_coordinate(optarg, config))
+                                return false;
+                        break;
+
+                case -1:
+                        goto done;
+
+                default:
+                        return false;
+                }
+        }
+
+done:
+        if (config->lat == DBL_MAX) {
+                config->lat = 45.767615;
+                config->lon = 4.834434;
+        } else if (config->lon == DBL_MAX) {
+                fprintf(stderr,
+                        "latitude specified without longitude\n");
+                return false;
+        }
+
+        return true;
+}
 
 int
 main(int argc, char **argv)
 {
-        double lat = 45.767615, lon = 4.834434;
+        struct config config;
 
-        if (argc >= 2) {
-                lat = strtod(argv[1], NULL);
-
-                if (argc >= 3)
-                        lon = strtod(argv[2], NULL);
-        }
+        if (!process_options(argc, argv, &config))
+                return EXIT_FAILURE;
 
         int ret = EXIT_SUCCESS;
 
         cairo_surface_t *surface =
                 cairo_image_surface_create(CAIRO_FORMAT_RGB24,
-                                           IMAGE_SIZE, IMAGE_SIZE);
+                                           config.width,
+                                           config.height);
         cairo_t *cr = cairo_create(surface);
 
         struct flt_map_renderer *renderer =
-                flt_map_renderer_new(NULL, /* map_url_base */
-                                     NULL /* api_key */);
+                flt_map_renderer_new(config.url_base,
+                                     config.api_key);
 
         struct flt_error *error = NULL;
 
+        flt_map_renderer_set_clip(renderer, config.clip);
+
         if (!flt_map_renderer_render(renderer,
                                      cr,
-                                     17, /* zoom */
-                                     lat, lon,
-                                     IMAGE_SIZE / 2.0,
-                                     IMAGE_SIZE / 2.0,
-                                     IMAGE_SIZE, IMAGE_SIZE,
+                                     config.zoom,
+                                     config.lat,
+                                     config.lon,
+                                     config.width / 2.0,
+                                     config.height / 2.0,
+                                     config.width,
+                                     config.height,
                                      &error)) {
                 fprintf(stderr, "%s\n", error->message);
                 ret = EXIT_FAILURE;
                 flt_error_free(error);
-        } else {
-                cairo_surface_write_to_png(surface, "map.png");
+        } else if (cairo_surface_write_to_png(surface,
+                                              config.output_filename) !=
+                   CAIRO_STATUS_SUCCESS) {
+                fprintf(stderr, "error saving png\n");
+                ret = EXIT_FAILURE;
         }
 
         flt_map_renderer_free(renderer);
